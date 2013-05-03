1d830583dfaf742bc2e8d3fad2a42e062c2fa06c
Opryshko Evhen Igorevich <trancesmile@gmail.com>
2013-05-03 11:57:32 +0300

    Added Ukrainian message translations

---
+++ messages/uk/msg.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * @version 1.0
+ * @package Comments module
+ */
+return array(
+    'Comments'=>'Коментрарі',
+    'delete'=>'Видалити',
+    'approve'=>'затвердити',
+    'Approve'=>'Затвердити',
+    'You cannot add a new comment'=>'Вы не можете залишити коментар',
+    'No comments'=>'Коментарів немає',
+    'Please enter the letters as they are shown in the image above.<br/>Letters are not case-sensitive.'=>'Будь-ласка, введіть символи, вказані на картинці<br/>Символи регістро-незалежні',
+    'User Name'=>'Ім\'я користувача',
+    'User Email'=>'Email користувача',
+    'Comment Text'=>'Текст коментаря',
+    'Create Time'=>'Дата створення',
+    'Update Time'=>'Дата редагування',
+    'Status'=>'Статус повідомлення',
+    'Verification Code'=>'Код превірки',
+    //діалогове вікно
+    'Add comment'=>'Додати коментар',
+    'Delete this comment?'=>'Видалити коментар?',
+    'Approve this comment?'=>'Затвердити комментар?',
+    'Cancel'=>'Скасувати',
+    //адмін сторінка
+    'Manage Comments'=>'Управління комментарями',
+    'Manage'=>'Управління',
+    'Commented object'=>'Об\'єкт, що коментується',
+    'Commented object\'s ID'=>'ID об\'єкта, що коментується',
+    'New'=>'Новий',
+    'Approved'=>'Затвердженний',
+    'Deleted'=>'Удален',
+    'Link'=>'Посилання',
+);
+?>
